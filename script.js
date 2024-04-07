d0d319b7ff7b4ef3331dcc83988bbf8d319d7d39
poornima reddy <54319456+Poornimait@users.noreply.github.com>
2024-04-07 19:18:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function makeid(l) {
   // write your code here
+	let result = '';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let Length = characters.length;
+    for ( let i = 0; i < l; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * Length));
+    }
+    return result;
 }
 
 // Do not change the code below.
